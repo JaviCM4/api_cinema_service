a004b851da03bfa8516951862c803fb4d21f8123
JaviCM4 <javic4m@gmail.com>
2026-05-14 11:53:52 -0600

    feat: Creation and updating of cinema apart from the acquisition of financial services

---
+++ src/main/java/com/example/cinema/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package com.example.cinema.exceptions;
+
+public class ResourceNotFoundException extends Exception {
+
+    public ResourceNotFoundException() {}
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
