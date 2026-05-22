937bfacc8cd36f288f4f6e3fbc38a1b9bdd91c06
Rudy Reyes <rudyreyes202031213@cunoc.edu.gt>
2026-05-22 02:42:50 -0600

    feat: implement create cinema event

---
+++ src/main/java/com/example/cinema/kafka/CinemaEventConsumer.java
@@ -0,0 +1,29 @@
+package com.example.cinema.kafka;
+
+import com.example.cinema.events.cinema.CinemaCreatedEvent;
+import com.example.cinema.repositories.cinema.CinemaRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CinemaEventConsumer {
+    private static final String USER_EVENTS_TOPIC = "user-events";
+    private final CinemaRepository cinemaRepository;
+    private final ObjectMapper objectMapper;
+
+    public CinemaEventConsumer(CinemaRepository cinemaRepository, ObjectMapper objectMapper) {
+        this.cinemaRepository = cinemaRepository;
+        this.objectMapper = objectMapper;
+    }
+
+    @KafkaListener(topics = USER_EVENTS_TOPIC, groupId = "cinema-service-group")
+    public void onAdvertiserCreated(String payload) throws JsonProcessingException {
+        CinemaCreatedEvent event = objectMapper.readValue(payload, CinemaCreatedEvent.class);
+        if ("CINEMA_ADMIN_CREATED".equals(event.getEvent())) {
+            cinemaRepository.save(event.fromEvent());
+        }
+    }
+
+}
